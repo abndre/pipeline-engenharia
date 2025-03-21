b6c746168207c80c0d32e919a7690699dd8dc78c
Andre <Andre.Silva3@ext.rumolog.com>
2025-03-21 13:41:42 -0300

    update

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # pipeline-engenharia
+
+
+Initialize the project
+```
+docker compose up -d
+```
+
+Access the airflow webpage:
+
+http://localhost:8080/
+
+airflow:airflow
+
+Access the minins3 webpage
+
+http://localhost:9001/
+
+admin:password
+
+## Airflow Con
+
+atualize a connection do airflow: aws_default, adicione no extra
+
+```
+{
+  "aws_access_key_id": "admin",
+  "aws_secret_access_key": "password",
+  "endpoint_url": "http://minio:9000"
+}
+```
--- dags/my_dag.py
+++ dags/my_dag.py
@@ -8,7 +8,7 @@ from airflow.operators.python import PythonOperator
 from airflow.providers.amazon.aws.operators.s3 import S3ListOperator
 import pandas as pd
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
-import os
+import os, io
 # Configurações do MinIO/S3
 AWS_ACCESS_KEY_ID = "admin"
 AWS_SECRET_ACCESS_KEY = "password"
@@ -16,21 +16,34 @@ S3_ENDPOINT_URL = "http://minio:9000"
 BUCKET_NAME = "warehouse"
 OBJECT_NAME = "data/dados.csv"
 
+def read_csv_from_s3():
+    # Criar instância do S3Hook
+    s3_hook = S3Hook(aws_conn_id="aws_default")
+    
+    # Obter o arquivo do S3 como um objeto em memória
+    file_obj = s3_hook.get_key(key=OBJECT_NAME, bucket_name=BUCKET_NAME)
+
+    # Converter o conteúdo para um arquivo em memória
+    file_stream = io.StringIO(file_obj.get()['Body'].read().decode('utf-8'))
+
+    # Ler o CSV com Pandas
+    df = pd.read_csv(file_stream)
+    print(df)
+
 def create_and_upload_dataframe():
     # Criar DataFrame
     data = {"Nome": ["Alice", "Bob", "Charlie"], "Idade": [25, 30, 35]}
     df = pd.DataFrame(data)
 
-    # Salvar como CSV em um arquivo temporário
-    temp_file = "/tmp/dados.csv"
-    df.to_csv(temp_file, index=False)
+    # Salvar como CSV em um buffer de memória
+    csv_buffer = io.StringIO()
+    df.to_csv(csv_buffer, index=False)
+    csv_buffer.seek(0)  # Reposicionar o ponteiro no início do buffer
 
-    # Upload para o S3
+    # Upload para o S3 usando load_bytes
     s3_hook = S3Hook(aws_conn_id="aws_default")
-    s3_hook.load_file(filename=temp_file, bucket_name=BUCKET_NAME, key=OBJECT_NAME, replace=True)
+    s3_hook.load_bytes(bytes_data=csv_buffer.getvalue().encode('utf-8'), bucket_name=BUCKET_NAME, key=OBJECT_NAME, replace=True)
 
-    # Remover arquivo temporário
-    os.remove(temp_file)
     print(f"Arquivo {OBJECT_NAME} enviado para {BUCKET_NAME}")
 
 
@@ -85,5 +98,11 @@ with DAG(
             python_callable=create_and_upload_dataframe
         )
 
-    upload_task
-run_this >> list_task >> list_files >> upload_task
+
+    read_task = PythonOperator(
+        task_id="read_csv_from_s3",
+        python_callable=read_csv_from_s3
+    )
+
+    
+run_this >> list_task >> list_files >> upload_task >> read_task
